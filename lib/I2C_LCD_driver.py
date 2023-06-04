8cbc6d6d75739b50743adbf3427f13cd22249af9
dr-prodigy <dr.prodigy.github@gmail.com>
2023-06-04 14:11:47 +0200

    Revert "LCD not clear on init"

--- lib/I2C_LCD_driver.py
+++ lib/I2C_LCD_driver.py
@@ -139,8 +139,7 @@ class lcd:
 
         self.lcd_write(LCD_FUNCTIONSET | LCD_2LINE | LCD_5x8DOTS | LCD_4BITMODE)
         self.lcd_write(LCD_DISPLAYCONTROL | LCD_DISPLAYON)
-        # DON'T CLEAR (avoid flicker on refresh)
-        # self.lcd_write(LCD_CLEARDISPLAY)
+        self.lcd_write(LCD_CLEARDISPLAY)
         self.lcd_write(LCD_ENTRYMODESET | LCD_ENTRYLEFT)
         sleep(0.2)
 
