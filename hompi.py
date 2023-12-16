905fa41c6717f3568f8e244dc54b160f89b18128
Maurizio Montel <dr.prodigy.github@gmail.com>
2023-12-16 17:37:20 +0100

    Ambient review - beta

--- hompi.py
+++ hompi.py
@@ -80,7 +80,7 @@ task_every_secs = {
     'get_temp': 20.0,
     'get_meteo': 300.0,  # 5 mins
     'get_aphorism': 241.0,  # 4 mins
-    'refresh': 10.0,  # 2 min (multiple of get_temp)
+    'refresh': 120.0,  # 2 min (multiple of get_temp)
     'update_temp': 80.0,
     'update_io': 10.0,
 }
