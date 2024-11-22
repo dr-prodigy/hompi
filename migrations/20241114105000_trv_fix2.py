007734d9d2408c10030d96b3e1d5576ba1ec0068
dr-prodigy <dr.prodigy.github@gmail.com>
2024-11-22 10:24:00 +0100

    Smartworking morning - main temp fix

--- migrations/20241114105000_trv_fix2.py
+++ migrations/20241114105000_trv_fix2.py
@@ -8,7 +8,11 @@ version: 20241114105000
 
 
 def upgrade(connection):
-    sql = "UPDATE `gm_timetable_type_data` SET orderby = id - 30 WHERE `id` >= 30"
+    sql = "UPDATE `gm_timetable_type_data` SET `orderby` = `id` - 30 WHERE `id` >= 30"
+    connection.execute(sql)
+    connection.commit()
+
+    sql = "UPDATE `gm_timetable_type_data` SET `temp_id` = 2 WHERE `id` = 32"
     connection.execute(sql)
     connection.commit()
 
