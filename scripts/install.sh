ff99cf79d291c6d9533c73e2969e964cc75bd666
Maurizio Montel <dr.prodigy.github@gmail.com>
2025-07-03 23:04:52 +0200

    installation fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,15 +16,17 @@
 # You should have received a copy of the GNU General Public License
 # along with hompi.  If not, see <http://www.gnu.org/licenses/>.
 
-# install prerequisites
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
+## install prerequisites - 20250703 - not required
+## sudo apt-get install python3-pip
+## sudo pip3 install virtualenv
 
 # move to script directory
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # move up
 cd ..
 # create virtualenv, activate it, setup requirements
-python3 -m virtualenv venv
+## 20250703 - replaced with venv
+## python3 -m virtualenv venv
+python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements/requirements.txt
